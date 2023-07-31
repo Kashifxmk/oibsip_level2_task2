f7e23f7b814b6c4fe4bf67c458e40b0ef8891a7b
Kashifxmk <96860166+Kashifxmk@users.noreply.github.com>
2023-07-31 13:47:44 +0530

    Add files via upload

---
+++ heroo/apj.js
@@ -0,0 +1,8 @@
+// typed js 
+const typed= new Typed('.multiple-text',{
+    strings:['You have to dream before the dream comes true.','“We must think and act like a nation of a billion people and not like that of a million people.Dream, dream, dream!” ','Thinking is the capital, Enterprise is the way, Hard Work is the solution.'],
+    typeSpeed:60,
+    backSpeed:10,
+    backDelay:3000,
+    loop:true
+});
